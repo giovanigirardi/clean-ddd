1dccbb0b6d5204d8274cd3e69db692912f1713e2
Giovani Girardi <giovanigg08@gmail.com>
2024-09-22 17:48:13 -0300

    refactor: :goal_net: created Either class

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,3 +1,4 @@
+import { type Either, left, right } from "@/core/either";
 import type { AnswerCommentsRepository } from "../repositories/answer-comments-repository";
 
 interface DeleteAnswerCommentUseCaseRequest {
@@ -5,22 +6,27 @@ interface DeleteAnswerCommentUseCaseRequest {
 	answerCommentId: string;
 }
 
+type DeleteAnswerCommentUseCaseResponse = Either<string, { [k: string]: never }>;
+
 export class DeleteAnswerCommentUseCase {
 	constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
-	async execute({ authorId, answerCommentId }: DeleteAnswerCommentUseCaseRequest) {
+	async execute({
+		authorId,
+		answerCommentId,
+	}: DeleteAnswerCommentUseCaseRequest): Promise<DeleteAnswerCommentUseCaseResponse> {
 		const answerComment = await this.answerCommentsRepository.findById(answerCommentId);
 
 		if (!answerComment) {
-			throw new Error("Answer comment not found");
+			return left("Answer comment not found");
 		}
 
 		if (answerComment.authorId.toString() !== authorId) {
-			throw new Error("You can only delete your own comments");
+			return left("You can only delete your own comments");
 		}
 
 		await this.answerCommentsRepository.delete(answerComment);
 
-		return {};
+		return right({});
 	}
 }
