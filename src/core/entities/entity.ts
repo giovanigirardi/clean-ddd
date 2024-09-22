7876beec2ff52bb1d34b3b152437e3f05bf3cb0c
Giovani Girardi <giovanigg08@gmail.com>
2024-09-21 21:49:26 -0300

    feat: :sparkles: created unique entity id value object

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -1,15 +1,15 @@
-import { randomUUID } from "node:crypto";
+import { UniqueEntityId } from "./unique-entity-id";
 
 export class Entity<T> {
-	private _id: string;
+	private _id: UniqueEntityId;
 	protected props: T = {} as T;
 
-	get id(): string {
+	get id(): UniqueEntityId {
 		return this._id;
 	}
 
 	constructor(props: T, id?: string) {
 		this.props = props;
-		this._id = id ?? randomUUID();
+		this._id = new UniqueEntityId(id);
 	}
 }
